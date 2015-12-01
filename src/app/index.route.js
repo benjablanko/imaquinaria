40b40b77d24eef5d0f3ff4d162da42d10d33be76
benjamin blank <benjamin blank>
2015-11-30 22:34:32 -0300

    first commit

---
+++ src/app/index.route.js
@@ -0,0 +1,35 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('miApp')
+    .config(routerConfig);
+
+  /** @ngInject */
+  function routerConfig($stateProvider, $urlRouterProvider) {
+    $stateProvider
+      .state('home', {
+        url: '/',
+        views: {
+            // the main template will be placed here (relatively named)
+            'nav': {
+              templateUrl: 'app/nav/nav.html'
+            },
+            '': { 
+              templateUrl: 'app/main/main.html',
+              controller: 'MainController',
+              controllerAs: 'main'
+            },
+            // the child views will be defined here (absolutely named)
+            'producto': {
+              templateUrl: 'app/producto/producto.html'//,
+              //controller: 'ProductoController',
+              //controllerAs: 'producto'
+            }
+        }
+      });
+
+    $urlRouterProvider.otherwise('/');
+  }
+
+})();
